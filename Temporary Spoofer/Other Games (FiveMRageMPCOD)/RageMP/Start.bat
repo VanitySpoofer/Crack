ff770db280f70b7899a8785cac754244dddd9d97
VanitySpoofer <98795744+VanitySpoofer@users.noreply.github.com>
2022-01-31 21:59:38 -0800

    Add files via upload

---
+++ Temporary Spoofer/Other Games (FiveMRageMPCOD)/RageMP/Start.bat
@@ -0,0 +1,20 @@
+@echo off
+title I like Migos ATL
+CD C:\Windows
+KernelMapper.exe /su auto
+KernelMapper.exe /bsh %random%-%random%-%random%
+KernelMapper.exe /bhm %random%-%random%-%random%
+KernelMapper.exe /CSH %random%-%random%-%random%
+KernelMapper.exe /CS %random%-%random%-%random%
+KernelMapper.exe /CSK %random%-%random%-%random%
+KernelMapper.exe /CA %random%-%random%-%random%
+KernelMapper.exe /ss %random%-%random%-%random%
+KernelMapper.exe /bs %random%-%random%-%random%
+KernelMapper.exe /bsh %random%-%random%-%random%
+KernelMapper.exe /cs %random%-%random%-%random%
+KernelMapper.exe /ca %random%-%random%-%random%
+KernelMapper.exe /psn %random%-%random%
+KernelMapper.exe /ppn %random%-%random%
+KernelMapper.exe /pat %random%-%random%
+KernelMapper.exe /ps %random%-%random%
+exit
